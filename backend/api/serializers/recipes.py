d959f988105192444da11745410d33e56b0d3c3c
Sergey Zhuchkov <Serejasky@gmail.com>
2023-07-14 18:44:43 +0300

    foodgram_v.3.1

--- backend/api/serializers/recipes.py
+++ backend/api/serializers/recipes.py
@@ -1,7 +1,6 @@
 from django.db import transaction
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from recipes.models import (
     Favorite,
@@ -247,4 +246,3 @@ class ShoppingCartSerializer(serializers.ModelSerializer):
             instance.recipe,
             context={'request': self.context.get('request')}
         ).data
-
